b23cfe1292cc9fdaa99aa901fdcdea6036c8c853
geetmehta989 <103765083+geetmehta989@users.noreply.github.com>
2025-09-20 01:12:04 +0530

    app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { LoggerModule } from 'nestjs-pino';
-import { buildTypeOrmConfig } from './database/typeorm.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HeadingsModule } from './headings/headings.module';
-import { CategoriesModule } from './categories/categories.module';
-import { ProductsModule } from './products/products.module';
-import { ScrapeModule } from './scrape/scrape.module';
 
 @Module({
   imports: [
-    LoggerModule.forRoot(),
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      useFactory: () => buildTypeOrmConfig() as unknown as TypeOrmModuleOptions,
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
-    HeadingsModule,
-    CategoriesModule,
-    ProductsModule,
-    ScrapeModule,
   ],
   controllers: [AppController],
   providers: [AppService],
